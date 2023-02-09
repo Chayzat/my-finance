3081eeff6f65566360c2c82f3ed8b79a10620785
chayzat <chayzat.oorzhak.2001@mail.ru>
2023-02-09 19:11:48 +0300

    feat: basic template

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,71 @@ import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <header class="header" id="header">
+        <div class="heading container flex">
+            <h1 class="heading__title">Мои финансы</h1>
+            <button class="heading__btn btn">Добавить</button>
+        </div>
+    </header>
+    <main class="main">
+         <section class="account section">
+            <div class="card container grid">
+                <div class="card__body">
+                    <div class="card__body-header flex">
+                        <h4 class="card__body-title">Всего:</h4>
+                        <div class="card__body-stat">
+                            200 <span class="card__body-probability"> / 7800</span>
+                        </div>
+                    </div>
+                    <div class="card__body-progress flex">
+                        <div class="card__body-progressbar">
+
+                        </div>
+                    </div>
+                    <div class="card__body-btn flex">
+                        <button class="card__body-add btn">Добавить</button>
+                        <button class="card__body-see btn">Посмотреть</button>
+                    </div>
+                </div>
+                <div class="card__body">
+                    <div class="card__body-header flex">
+                        <h4 class="card__body-title">Всего:</h4>
+                        <div class="card__body-stat">
+                            200 <span class="card__body-probability"> / 7800</span>
+                        </div>
+                    </div>
+                    <div class="card__body-progress flex">
+                        <div class="card__body-progressbar">
+
+                        </div>
+                    </div>
+                    <div class="card__body-btn flex">
+                        <button class="card__body-add btn">Добавить</button>
+                        <button class="card__body-see btn">Посмотреть</button>
+                    </div>
+                </div>
+                <div class="card__body">
+                    <div class="card__body-header flex">
+                        <h4 class="card__body-title">Всего:</h4>
+                        <div class="card__body-stat">
+                            200 <span class="card__body-probability"> / 7800</span>
+                        </div>
+                    </div>
+                    <div class="card__body-progress flex">
+                        <div class="card__body-progressbar">
+
+                        </div>
+                    </div>
+                    <div class="card__body-btn flex">
+                        <button class="card__body-add btn">Добавить</button>
+                        <button class="card__body-see btn">Посмотреть</button>
+                    </div>
+                </div>
+            </div>
+         </section>
+    </main>
+    </div>
   )
 }
 
